09022e8f35783c5b03277b7444c78e87d71e2140
Bugra Üçkuş <bugra3kus@gmail.com>
2025-03-13 11:17:47 +0300

    update pom.xml file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,11 +2,20 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"stepDefinitions", "hooks"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        glue = "SearchSteps",
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
